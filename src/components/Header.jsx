a47250a3cb8d1580ad75608372896a24cf10f58e
Prashant Kumar <programmerkrprashant@gmail.com>
2025-03-27 19:14:32 +0530

    copy text-color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import Nav from './Nav'
 export default function Header () {
   return (
     <div className='w-full fixed top-0 left-0 z-50 bg-white flex justify-between items-center md:flex-col'>
-      <div className='flex items-center justify-center'>
+      <div className='flex items-center justify-center py-3'>
         <img src={logo} alt='logo' className='size-20 md:size-26' />
         <h1 className='font-bold text-2xl leading-[0.8] md:text-3xl text-sky-700'>
           MAX PUBLIC <br /> SCHOOL
