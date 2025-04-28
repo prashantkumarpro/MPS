866c8da0112a3fc9c9e20505f33e5c8db0d4403d
Prashant Kumar <programmerkrprashant@gmail.com>
2025-04-28 18:57:39 +0530

    added teacher_img

---
+++ src/data/profiles.js
@@ -0,0 +1,89 @@
+// This file contains a list of teacher profiles with their details.
+// Each profile includes an ID, name, role, and image path.
+
+const profiles = [
+  {
+    id: 13,
+    name: 'Alok Ratn',
+    role: 'Mnaging Director',
+    image: '/teachers/Alok_Ratn.png'
+  },
+  {
+    id: 6,
+    name: 'Shohel Akhtar',
+    role: 'Principal',
+    image: '/teachers/Shohel_Akhtar.png'
+  },
+  {
+    id: 12,
+    name: 'Rajesh Ranjan',
+    role: 'Teacher',
+    image: '/teachers/Rajesh.png'
+  },
+  {
+    id: 8,
+    name: 'Tribhuvan Malakar',
+    role: 'Teacher',
+    image: '/teachers/Imran_khan.png'
+  },
+  {
+    id: 9,
+    name: 'Tribhuvan Thakur',
+    role: 'Teacher',
+    image: '/teachers/ttsir.enc'
+  },
+  {
+    id: 1,
+    name: 'Kanchan Kumari',
+    role: 'Teacher',
+    image: '/teachers/kanchan.png'
+  },
+  {
+    id: 2,
+    name: 'Meena Kumari',
+    role: 'Teacher',
+    image: '/teachers/Meena_kumari.png'
+  },
+  {
+    id: 3,
+    name: 'Imaran Khan',
+    role: 'Teacher',
+    image: '/teachers/Imran_khan.png'
+  },
+  {
+    id: 4,
+    name: 'Bipin Kumar',
+    role: 'Teacher',
+    image: '/teachers/Bipin_kumar.png'
+  },
+  {
+    id: 5,
+    name: 'Amresh Pandey',
+    role: 'Teacher',
+    image: '/teachers/Ashish_kumar.png'
+  },
+
+
+
+  {
+    id: 10,
+    name: 'Bittu Kumar',
+    role: 'Accountent',
+    image: '/teachers/bittu.enc'
+  },
+  {
+    id: 11,
+    name: 'Ashish Kumar',
+    role: 'Teacher',
+    image: '/teachers/Ashish_kumar.png'
+  },
+  {
+    id: 7,
+    name: 'Prashant Kumar',
+    role: 'Teacher',
+    image: '/teachers/Prashant.jpg'
+  },
+  // ...keep adding up to 15
+]
+
+export default profiles
