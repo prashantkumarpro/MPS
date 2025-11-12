2429769ea75d45d479a7378b2ec4122209c7b2a1
Prashant Kumar <programmerkrprashant@gmail.com>
2025-11-12 22:30:11 +0530

    remove npmrc

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const app = express()
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3000
 
 // Middleware - allow your frontend domain
 app.use(
