659c1dd66c71d62dafa1556d7c650d94506544c8
Prashant Kumar <programmerkrprashant@gmail.com>
2025-11-23 20:00:45 +0530

    update package.json, app.js

--- backend/app.js
+++ backend/app.js
@@ -21,6 +21,8 @@ app.get('/', (req, res) => {
   res.send('MPS Backend Running')
 })
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`)
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
