52745b0cda495f469ce0cfd15eb826a959fb5bc0
Prashant Kumar <programmerkrprashant@gmail.com>
2025-12-11 19:45:00 +0530

    update checkResult page

--- frontend/src/pages/CheckResult.jsx
+++ frontend/src/pages/CheckResult.jsx
@@ -44,7 +44,7 @@ const CheckResult = () => {
         type='text'
         placeholder='Enter your class'
         value={studentClass}
-        onChange={e => setStudentClass(e.target.value.toUpperCase())}
+        onChange={e => setStudentClass(e.target.value)}
         className='px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500'
       />
 
