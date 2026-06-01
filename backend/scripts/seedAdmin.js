d64e444ea682b936712fbaa433f18a18750c1248
Prashant Kumar <programmerkrprashant@gmail.com>
2026-06-01 17:44:04 +0530

    add login

---
+++ backend/scripts/seedAdmin.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import User from '../models/User.js'
+
+dotenv.config()
+
+const createAdmin = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI)
+
+    const existingAdmin = await User.findOne({
+      email: 'mpsadmin@mps.com'
+    })
+
+    if (existingAdmin) {
+      console.log('Admin already exists')
+      process.exit()
+    }
+
+    const admin = new User({
+      name: 'devPrashant',
+      email: 'mpsadmin@mps.com',
+      password: '',
+      role: 'admin'
+    })
+
+    await admin.save()
+
+    console.log('Admin created successfully')
+    process.exit()
+  } catch (error) {
+    console.error(error)
+    process.exit(1)
+  }
+}
+
+createAdmin()
